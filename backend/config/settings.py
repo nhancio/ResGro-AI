82fbc4fe1bba73a92d1ee6e8127a5b7c2c560c44
Didigam Nithin <nithindidigam@gmail.com>
2026-05-27 18:39:41 +0530

    pre-deploy: 2026-05-27 18:39:41

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -153,3 +153,15 @@ RESGRO_ADMIN_EMAILS = [
     ).split(",")
     if e.strip()
 ]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {"handlers": ["console"], "level": "WARNING"},
+    "loggers": {
+        "django.request": {"handlers": ["console"], "level": "ERROR", "propagate": False},
+    },
+}
